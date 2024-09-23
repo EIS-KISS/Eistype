58673d0a73e9cd6f64bda1b99f9b405ae4b992e1
Carl Philipp Klemm <philipp@uvos.xyz>
2024-09-23 14:24:33 +0200

    make the test application print the lib version, so that it at least dose _something_

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
+#include "kisstype/type.h"
+#include <iostream>
 
 int main(int argc, char** argv)
 {
+	VersionFixed version = eis_get_version();
+	std::cout<<"version: "<<version.major<<'.'<<version.minor<<'.'<<version.patch<<'\n';
 	return 0;
 }
